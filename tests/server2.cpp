40fc05fcef0a2b0148cb2362fcd3649748d33bbf
Alessandro Pignotti <a.pignotti@sssup.it>
2012-07-15 18:27:45 +0200

    Improve test case

--- tests/server2.cpp
+++ tests/server2.cpp
@@ -4,9 +4,21 @@
 
 #include <iostream>
 
-void serverTest(int) [[server]]
+void serverTest(int a) [[server]]
 {
 	std::cout << "CALLED ON THE SERVER" << std::endl;
+	std::cerr << "INT2 " << a << std::endl;
+}
+
+void serverTest2() [[server]]
+{
+	std::cerr << "NO ARGS" << std::endl;
+}
+
+void serverTest3(int a, float f) [[server]]
+{
+	std::cerr << "INT " << a << std::endl;
+	std::cerr << "FLOAT " << f << std::endl;
 }
 
 void clientTest() [[client]]
@@ -16,4 +28,7 @@ void clientTest() [[client]]
 
 void webMain() [[client]]
 {
+	serverTest(9);
+	serverTest2();
+	serverTest3(2, 9.2);
 }
