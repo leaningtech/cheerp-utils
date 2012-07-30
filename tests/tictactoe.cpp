f953b7295f2464cd7c782e0fe5770baa4136cfad
Alessandro Pignotti <a.pignotti@sssup.it>
2012-07-30 16:47:47 +0200

    Initial work on tictactoe demo

---
+++ tests/tictactoe.cpp
@@ -0,0 +1,52 @@
+#include "client.h"
+#include "server.h"
+#include "canvas.h"
+
+#include <iostream>
+#include <vector>
+
+using namespace client;
+
+//TODO: enforce access only on server code
+struct GameState
+{
+	bool state[9];
+};
+
+static GameState gameState;
+
+inline int serverTest() [[server]]
+{
+//	return "String created on the server";
+	std::cerr << "SERVER SIDE" << std::endl;
+	return 42;
+}
+
+void clientTest() [[client]]
+{
+	Document* d=Client::get_document();
+	Element* e=d->getElementById("canvas");
+	//TODO: support dynamic_cast
+	HTMLCanvasElement* canvas=static_cast<HTMLCanvasElement*>(e);
+	CanvasRenderingContext2D* ctx=static_cast<CanvasRenderingContext2D*>(canvas->getContext("2d"));
+	ctx->moveTo(0,0);
+	ctx->lineTo(100,100);
+	ctx->stroke();
+}
+
+void webMain() [[client]]
+{
+//	serverTest();
+}
+
+inline void resetGame() [[server]]
+{
+	for(int i=0;i<9;i++)
+		gameState.state[i]=false;
+}
+
+void serverMain() [[server]]
+{
+	std::cerr << "Initializing game" << std::endl;
+	resetGame();
+}
