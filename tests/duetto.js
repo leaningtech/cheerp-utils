461ce889fc514eeb22954119ce302905f9096e18
Alessandro Pignotti <a.pignotti@sssup.it>
2012-12-09 15:03:36 +0100

    Add support for duetto compiler in the JS support code

--- tests/duetto.js
+++ tests/duetto.js
@@ -2,11 +2,20 @@
 
 function __duettoCreateBuiltin_ZN6client6StringC1EPKc(a)
 {
-	//This depends on emscripten implementation
-	var ret = new String();
-	for(;HEAP8[a]!=0;a++)
-		ret += String.fromCharCode(HEAP8[a]);
-	return ret;
+	if(__duetto_compiler==true)
+	{
+		//The pointer is a const char*, so we expect
+		//the referenced data to be a String
+		return a.d.substr(a.o);
+	}
+	else
+	{
+		//This depends on emscripten implementation
+		var ret = new String();
+		for(;HEAP8[a]!=0;a++)
+			ret += String.fromCharCode(HEAP8[a]);
+		return ret;
+	}
 }
 
 function __duettoCreateBuiltin_ZN6client6StringC1Ei(a)
@@ -17,8 +26,13 @@ function __duettoCreateBuiltin_ZN6client6StringC1Ei(a)
 //Passthrough code to pass event handlers in a semi type safe manner
 function __duettoCreateBuiltin_ZN6client8CallbackC1EPFvvE(f)
 {
-	var ret=FUNCTION_TABLE[f];
-	return ret;
+	if(__duetto_compiler==true)
+		return f;
+	else
+	{
+		var ret=FUNCTION_TABLE[f];
+		return ret;
+	}
 }
 
 //Conversion operator from generic object to Number/double
