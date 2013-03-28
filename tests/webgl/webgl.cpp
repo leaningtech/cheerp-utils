df67272e7bf5a44c60a8344f1be0ca5d74fa15b6
Alessandro Pignotti <a.pignotti@sssup.it>
2013-03-28 14:20:32 +0100

    webgl: Use auto keyword

--- tests/webgl/webgl.cpp
+++ tests/webgl/webgl.cpp
@@ -6,8 +6,8 @@ using namespace client;
 
 void loadCallback() [[client]]
 {
-	HTMLCanvasElement* canvas=document->getElementById<HTMLCanvasElement>("glcanvas");
-	WebGLRenderingContext* gl=canvas->getContext<WebGLRenderingContext>("experimental-webgl");
+	auto canvas=document->getElementById<HTMLCanvasElement>("glcanvas");
+	auto gl=canvas->getContext<WebGLRenderingContext>("experimental-webgl");
 	gl->clearColor(0,0,0,1);
 	gl->clear(gl->get_COLOR_BUFFER_BIT());
 }
