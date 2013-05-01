136364ef3638383cabd1cd974ca0fe6894fb4267
Alessandro Pignotti <a.pignotti@sssup.it>
2013-05-01 02:58:54 +0200

    tests: Retrofit webgl test to new headers

--- tests/webgl/webgl.cpp
+++ tests/webgl/webgl.cpp
@@ -1,13 +1,13 @@
 #include <duetto/client.h>
-#include <duetto/canvas.h>
+#include <duetto/clientlib.h>
 #include <duetto/webgl.h>
 
 using namespace client;
 
 void loadCallback() [[client]]
 {
-	auto canvas=document->getElementById<HTMLCanvasElement>("glcanvas");
-	auto gl=canvas->getContext<WebGLRenderingContext>("experimental-webgl");
+	auto canvas=static_cast<HTMLCanvasElement*>(document->getElementById("glcanvas"));
+	auto gl=reinterpret_cast<WebGLRenderingContext*>(canvas->getContext("experimental-webgl"));
 	gl->clearColor(0,0,0,1);
 	gl->clear(gl->get_COLOR_BUFFER_BIT());
 }
