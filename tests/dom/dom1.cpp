0c582cd47f8e730dc30c29cca5e7997229940fbd
Alessandro Pignotti <a.pignotti@sssup.it>
2013-05-01 11:05:12 +0200

    tests: Add simple DOM test

---
+++ tests/dom/dom1.cpp
@@ -0,0 +1,19 @@
+#include <duetto/client.h>
+#include <duetto/clientlib.h>
+#include <duetto/webgl.h>
+
+using namespace client;
+
+void loadCallback() [[client]]
+{
+	Element* body=document->get_body();
+	Element* newTile=document->createElement("h1");
+	newTile->set_textContent("Hello World");
+	body->appendChild(newTile);
+}
+
+int webMain() [[client]]
+{
+	document->addEventListener("DOMContentLoaded",Callback(loadCallback));
+	return 0;
+}
