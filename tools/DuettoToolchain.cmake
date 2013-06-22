7faef9a2ebad2328ad4892db28bcd69acb5778c9
Alessandro Pignotti <a.pignotti@sssup.it>
2013-06-22 21:00:40 +0200

    cmake: Add files for cmake support

---
+++ tools/DuettoToolchain.cmake
@@ -0,0 +1,21 @@
+INCLUDE(CMakeForceCompiler)
+
+list(APPEND CMAKE_MODULE_PATH "/opt/duetto/share/cmake/Modules")
+
+# this one is important
+SET(CMAKE_SYSTEM_NAME Duetto)
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+
+# specify the cross compiler
+CMAKE_FORCE_C_COMPILER(/opt/duetto/bin/clang GNU)
+CMAKE_FORCE_CXX_COMPILER(/opt/duetto/bin/clang GNU)
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH /opt/duetto)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
