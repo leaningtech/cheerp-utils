d664ed89a4562207a2f2e6af43f04a0367a45be0
Alessandro Pignotti <a.pignotti@sssup.it>
2013-07-03 22:32:16 +0200

    tools: Fix FLAGS declaration

--- tools/cmake/Duetto.cmake
+++ tools/cmake/Duetto.cmake
@@ -1,7 +1,7 @@
 set_property(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS FALSE)
 
-SET(CMAKE_C_FLAGS "-emit-llvm -target duetto")
-SET(CMAKE_CXX_FLAGS "-std=c++11 -emit-llvm -target duetto")
+SET(CMAKE_C_FLAGS_INIT "-emit-llvm -target duetto")
+SET(CMAKE_CXX_FLAGS_INIT "-std=c++11 -emit-llvm -target duetto")
 
 set(CMAKE_STATIC_LIBRARY_SUFFIX ".bc")
 set(CMAKE_CXX_CREATE_STATIC_LIBRARY "/opt/duetto/bin/llvm-link <OBJECTS> -o <TARGET>")
