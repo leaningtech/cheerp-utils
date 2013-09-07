1542ddd85200a59e45e9d56735c338b8def660e6
Alessandro Pignotti <a.pignotti@sssup.it>
2013-09-07 17:00:36 +0200

    Minor fix to a memory test

--- tests/unit/memory/test1.cpp
+++ tests/unit/memory/test1.cpp
@@ -23,7 +23,7 @@ void webMain()
 	int c[] = {0,0,0,0,0,0,0,0};
 
 	memmove(c,a,8*sizeof(int));
-	assertEqual(c[3], 4, "Memmove (native types) on isolated memory");
+	assertEqual(c[3], 4, "Memmove, (native types) on isolated memory");
 
 	memmove(a+2,a,6*sizeof(int));
 	assertEqual(a[3], 2, "Memmove, (native types) destination after source");
@@ -37,12 +37,11 @@ void webMain()
 	A oc[] = {0,0,0,0,0,0,0,0};
 
 	memmove(oc,oa,8*sizeof(A));
-	assertEqual(oc[3].b, 12, "Memmove (object types) on isolated memory");
+	assertEqual(oc[3].b, 12, "Memmove, (object types) on isolated memory");
 
 	memmove(oa+2,oa,6*sizeof(A));
 	assertEqual(oa[3].b, 10, "Memmove, (object types) destination after source");
 
 	memmove(ob,ob+2,6*sizeof(A));
 	assertEqual(ob[3].b, 14, "Memmove, (object types) destination before source");
-
 }
