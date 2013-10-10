81f5837fb03ce88a2f16f3b28d6ac0a84f49361c
Alessandro Pignotti <a.pignotti@sssup.it>
2013-10-10 19:07:45 +0200

    tests: Add more tests for unions

--- tests/unit/types/test6.cpp
+++ tests/unit/types/test6.cpp
@@ -3,6 +3,7 @@
 //===----------------------------------------------------------------------===//
 
 #include <tests.h>
+#include <string.h>
 
 union U
 {
@@ -14,8 +15,14 @@ union U
 void webMain()
 {
 	//Test union
-	U u;
+	U u, u2;
 	u.f = 1234.567;
 	assertEqual(u.i, 0x449A5225, "Access to unions 1/2");
 	assertEqual<char,char>(u.a[2], 0x9A, "Access to unions 2/2");
+
+	//Memory ops on unions
+	memcpy(&u2, &u, sizeof(u));
+	assertEqual(u2.i, 0x449A5225, "Memops on unions 1/2");
+	memset(&u2, 0, sizeof(u));
+	assertEqual(u2.i, 0, "Memops on unions 2/2");
 }
