8fb30193f684163cd23089bf3c42e50c6c2095e7
Alessandro Pignotti <a.pignotti@sssup.it>
2013-10-14 20:52:08 +0200

    tests: Stops the compiler from optimizing too much

--- tests/unit/memory/test2.cpp
+++ tests/unit/memory/test2.cpp
@@ -8,20 +8,20 @@
 void webMain()
 {
 	//Test legacy C memory allocation
-	int* a=(int*)malloc(2*sizeof(int));
+	int* volatile a=(int*)malloc(2*sizeof(int));
 	a[1] = 42;
 	assertEqual(a[1], 42, "Access malloc allocated memory");
 	free(a);
 
 	//Test C++ memory allocation
-	int* b=new int[2];
+	int* volatile b=new int[2];
 	b[1]=43;
 	assertEqual(b[1], 43, "Access new allocated memory");
 	delete[] b;
 
 	//Test variable sized C++ memory alllocation
 	volatile int n = 2;
-	int* c=new int[n];
+	int* volatile c=new int[n];
 	c[1]=44;
 	assertEqual(c[1], 44, "Access variable sized new allocated memory");
 	delete[] c;
