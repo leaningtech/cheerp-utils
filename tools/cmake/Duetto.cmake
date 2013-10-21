cb5e2258e20cfae34d014cca4da8858be314141d
Alessandro Pignotti <a.pignotti@sssup.it>
2013-10-21 22:06:37 +0200

    Improve cmake toolchain files to automatically link JS executables

--- tools/cmake/Duetto.cmake
+++ tools/cmake/Duetto.cmake
@@ -5,3 +5,6 @@ SET(CMAKE_CXX_FLAGS "-std=c++11 -emit-llvm -target duetto")
 
 set(CMAKE_STATIC_LIBRARY_SUFFIX ".bc")
 set(CMAKE_CXX_CREATE_STATIC_LIBRARY "/opt/duetto/bin/llvm-link <OBJECTS> -o <TARGET>")
+
+set(DUETTO_STANDARD_LIBRARIES "/opt/duetto/lib/libcxxabi.bc /opt/duetto/lib/libc.bc /opt/duetto/lib/libm.bc /opt/duetto/lib/libc++.bc")
+set(CMAKE_CXX_LINK_EXECUTABLE "/opt/duetto/bin/llvm-link <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES> ${DUETTO_STANDARD_LIBRARIES}" "/opt/duetto/bin/duetto-compiler <TARGET>")
