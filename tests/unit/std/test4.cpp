120b371c0032e1b9e7a293fb7e4737189313160f
Alessandro Pignotti <a.pignotti@sssup.it>
2013-12-14 01:26:39 +0100

    tests: Add iostream test

---
+++ tests/unit/std/test4.cpp
@@ -0,0 +1,14 @@
+//===---------------------------------------------------------------------===//
+//	Copyright 2013 Leaning Technlogies
+//===----------------------------------------------------------------------===//
+
+//#include <tests.h>
+#include <iostream>
+
+void webMain()
+{
+	//iostream and sstream test
+	std::cout << "Cout output : SUCCESS" << std::endl;
+	std::cerr << "Cerr output : SUCCESS" << std::endl;
+	//assertEqual(str.size(), 0, "String construction");
+}
