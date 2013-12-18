0edfe43ad8d17ca7f14fda1468b72045844bd600
Alessandro Pignotti <a.pignotti@sssup.it>
2013-12-18 14:22:34 +0100

    tests: Improve string test

--- tests/unit/std/test3.cpp
+++ tests/unit/std/test3.cpp
@@ -8,5 +8,9 @@
 void webMain()
 {
 	std::string str;
-	assertEqual(str.size(), 0, "String construction");
+	assertEqual(str.size(), 0, "String empty construction 1/2");
+	assertEqual(str.capacity(), 0, "String empty construction 2/2");
+	std::string str2("test");
+	assertEqual(str2.size(), 4, "String construction 1/2");
+	assertEqual(str2.capacity(), 9, "String construction 2/2");
 }
