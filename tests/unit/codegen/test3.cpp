1a3ff196c7ddd60d7fd2d70267da380bd427532c
Alessandro Pignotti <a.pignotti@sssup.it>
2014-02-23 18:26:32 +0100

    tests: Add a test for operators in constant expressions

---
+++ tests/unit/codegen/test3.cpp
@@ -0,0 +1,11 @@
+//===---------------------------------------------------------------------===//
+//	Copyright 2013 Leaning Technlogies
+//===----------------------------------------------------------------------===//
+
+#include <tests.h>
+
+void webMain()
+{
+	const char test[] = {0, 1, 4, 5*2};
+	assertEqual(test[2], 4, "Constant arrays in methods");
+}
