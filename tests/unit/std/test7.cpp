064e800e4e76f6f035529d7e22c720a35a3c9203
Alessandro Pignotti <a.pignotti@sssup.it>
2014-04-12 14:09:08 +0200

    tests: Add tests for unordered_set and unordered_map

--- tests/unit/std/test7.cpp
+++ tests/unit/std/test7.cpp
@@ -4,6 +4,7 @@
 
 #include <tests.h>
 #include <set>
+#include <unordered_set>
 
 void testSet()
 {
@@ -29,7 +30,7 @@ void testSet()
 	assertEqual(correctOrder, true, "std::set ordering 4/4");
 }
 
-void testMultiset()
+void testMultiSet()
 {
 	std::multiset<int> a;
 	assertEqual(a.size(), 0, "std::multiset 1/4");
@@ -53,8 +54,33 @@ void testMultiset()
 	assertEqual(correctOrder, true, "std::multiset ordering 4/4");
 }
 
+void testUnorderedSet()
+{
+	std::unordered_set<int> a;
+	assertEqual(a.size(), 0, "std::unordered_set 1/4");
+	a.insert(1);
+	assertEqual(a.size(), 1, "std::unordered_set 2/4");
+	a.insert(10);
+	a.insert(99);
+	a.insert(100);
+	a.insert(100);
+	a.insert(-2);
+	assertEqual(a.size(), 5, "std::unordered_set 3/4");
+	int expected[] = {-2, 99, 100, 10, 1};
+	std::unordered_set<int>::iterator it = a.begin();
+	bool correctOrder = true;
+	for(int i=0;i<a.size();i++)
+	{
+		if(*it != expected[i])
+			correctOrder = false;
+		++it;
+	}
+	assertEqual(correctOrder, true, "std::unordered_set ordering 4/4");
+}
+
 void webMain()
 {
 	testSet();
-	testMultiset();
+	testMultiSet();
+	testUnorderedSet();
 }
--- tests/unit/std/test8.cpp
+++ tests/unit/std/test8.cpp
@@ -4,6 +4,7 @@
 
 #include <tests.h>
 #include <map>
+#include <unordered_map>
 
 void testMap()
 {
@@ -29,7 +30,7 @@ void testMap()
 	assertEqual(correctOrder, true, "std::map ordering 4/4");
 }
 
-void testMultimap()
+void testMultiMap()
 {
 	std::multimap<int, int> a;
 	assertEqual(a.size(), 0, "std::multimap 1/4");
@@ -53,8 +54,33 @@ void testMultimap()
 	assertEqual(correctOrder, true, "std::multimap ordering 4/4");
 }
 
+void testUnorderedMap()
+{
+	std::unordered_map<int, int> a;
+	assertEqual(a.size(), 0, "std::unordered_map 1/4");
+	a.insert(std::make_pair(1,2));
+	assertEqual(a.size(), 1, "std::unordered_map 2/4");
+	a.insert(std::make_pair(10,11));
+	a.insert(std::make_pair(99,100));
+	a.insert(std::make_pair(100,101));
+	a.insert(std::make_pair(100,101));
+	a.insert(std::make_pair(-2, -1));
+	assertEqual(a.size(), 5, "std::unordered_map 3/4");
+	int expected[] = {-2, 99, 100, 10, 1};
+	std::unordered_map<int, int>::iterator it = a.begin();
+	bool correctOrder = true;
+	for(int i=0;i<a.size();i++)
+	{
+		if(it->first != expected[i] || it->second != expected[i]+1)
+			correctOrder = false;
+		++it;
+	}
+	assertEqual(correctOrder, true, "std::unordered_map ordering 4/4");
+}
+
 void webMain()
 {
 	testMap();
-	testMultimap();
+	testMultiMap();
+	testUnorderedMap();
 }
