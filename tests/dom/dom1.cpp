c87b8748eae923008f29a5a7e8c9ab37e9eff16f
Alessandro Pignotti <a.pignotti@sssup.it>
2014-04-14 01:30:00 +0200

    tests: Fix a typo

--- tests/dom/dom1.cpp
+++ tests/dom/dom1.cpp
@@ -6,9 +6,9 @@ using namespace client;
 void loadCallback() [[client]]
 {
 	HTMLElement* body=document.get_body();
-	HTMLElement* newTile=document.createElement("h1");
-	newTile->set_textContent("Hello World");
-	body->appendChild(newTile);
+	HTMLElement* newTitle=document.createElement("h1");
+	newTitle->set_textContent("Hello World");
+	body->appendChild(newTitle);
 }
 
 void webMain() [[client]]
