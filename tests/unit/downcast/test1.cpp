cf380fc70aa428ccd35e3b7ed4fb0a4a92b6e1c4
Alessandro Pignotti <a.pignotti@sssup.it>
2014-05-04 17:26:03 +0200

    tests: Add more tests for downcasts

--- tests/unit/downcast/test1.cpp
+++ tests/unit/downcast/test1.cpp
@@ -4,13 +4,19 @@
 
 #include <tests.h>
 
+struct C
+{
+	float c;
+	C():c(3){}
+};
+
 struct B
 {
 	int b;
 	B():b(1){}
 };
 
-struct A: public B
+struct A: public B, public C
 {
 	int a;
 	A():a(2){}
@@ -20,6 +26,9 @@ void webMain()
 {
 	A a;
 	B* volatile b=&a;
+	C* volatile c=&a;
 	A* volatile a2=static_cast<A*>(b);
-	assertEqual(a2->a, 2, "Downcast");
+	assertEqual(a2->a, 2, "Downcast 1/2");
+	A* volatile a3=static_cast<A*>(c);
+	assertEqual(a3->a, 2, "Downcast 2/2");
 }
