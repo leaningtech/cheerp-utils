2a57d2e51bf85db251deff12ad3a9d30f6710887
Alessandro Pignotti <a.pignotti@sssup.it>
2014-10-08 15:09:58 +0200

    include: Update copyright notice

--- include/client/client.h
+++ include/client/client.h
@@ -1,6 +1,6 @@
 /****************************************************************
  *
- * Copyright (C) 2012-2013 Alessandro Pignotti <alessandro@leaningtech.com>
+ * Copyright (C) 2012-2014 Alessandro Pignotti <alessandro@leaningtech.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
