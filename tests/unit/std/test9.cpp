283e7160100c169a148ed497729cf01bc03b2fdb
Alessandro Pignotti <a.pignotti@sssup.it>
2015-05-28 15:12:03 +0200

    Add a test case

---
+++ tests/unit/std/test9.cpp
@@ -0,0 +1,16 @@
+//===---------------------------------------------------------------------===//
+//	Copyright 2015 Leaning Technlogies
+//===----------------------------------------------------------------------===//
+
+#include <tests.h>
+#include <vector>
+
+void webMain()
+{
+	std::vector<float> v;
+	for(int i=0;i<10;i++)
+		v.push_back(i);
+	assertEqual(v.end()-v.begin(), 10, "std::vector 1/1");
+	v.erase(v.begin()+3);
+	assertEqual(v.end()-v.begin(), 9, "std::vector 2/2");
+}
