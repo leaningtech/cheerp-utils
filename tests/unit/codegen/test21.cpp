aad3ea8cfb1e547c9a40c375dee15e95bfa1533a
Sander Mathijs van Veen <smvv@kompiler.org>
2015-06-09 14:11:07 +0200

    Add test cases for int64_t add, sub, and unary ops

--- tests/unit/codegen/test21.cpp
+++ tests/unit/codegen/test21.cpp
@@ -4,6 +4,8 @@
 
 #include <tests.h>
 
+// Test 64-bit integer arithmetics
+
 static void dump(long long t) {
 	long h = (long)((t & (0xffffffffll << 32)) >> 32);
 	long l = (long)(t & 0xffffffff);
@@ -29,16 +31,16 @@ static void testShiftOps() {
 static void testBitwiseOps() {
 	long long a = 0xffffffffffffffffLL;
 	long long b = 0x0000000000000000LL;
-	long long lmask = 0x00000000ffffffffLL;
-	long long umask = 0xffffffff00000000LL;
+	long long l = 0x00000000ffffffffLL;
+	long long u = 0xffffffff00000000LL;
 
-	assertEqual(a & lmask, lmask, "int64_t and support 1/N");
-	assertEqual(a & umask, umask, "int64_t and support 2/N");
+	assertEqual(a & l, l, "int64_t and support 1/N");
+	assertEqual(a & u, u, "int64_t and support 2/N");
 
-	assertEqual(a | lmask, a, "int64_t or support 1/N");
-	assertEqual(b | lmask, lmask, "int64_t or support 2/N");
-	assertEqual(a | umask, a, "int64_t or support 3/N");
-	assertEqual(b | umask, umask, "int64_t or support 4/N");
+	assertEqual(a | l, a, "int64_t or support 1/N");
+	assertEqual(b | l, l, "int64_t or support 2/N");
+	assertEqual(a | u, a, "int64_t or support 3/N");
+	assertEqual(b | u, u, "int64_t or support 4/N");
 
 	assertEqual(a ^ b, a, "int64_t xor support 1/N");
 	assertEqual(a ^ a, b, "int64_t xor support 2/N");
@@ -46,7 +48,45 @@ static void testBitwiseOps() {
 
 	assertEqual(~a, b, "int64_t not support 1/N");
 	assertEqual(~b, a, "int64_t not support 2/N");
-	assertEqual(~lmask, umask, "int64_t not support 3/N");
+	assertEqual(~l, u, "int64_t not support 3/N");
+}
+
+static void testAddSubOps() {
+	long long a = 0x0deadbeaf;
+	long long b = 0x100000000;
+	long long c = 0x0ffffffff;
+	long long d = 0xffffffffffffffff;
+	long long e = 0xfffffffffffffffe;
+
+	assertEqual(a + b, 0x1deadbeaf, "int64_t add support 1/N");
+	assertEqual(c + 1, 0x100000000, "int64_t add support 2/N");
+	assertEqual(d + 1, 0x000000000, "int64_t add support 3/N");
+	assertEqual(d + d, e, "int64_t add support 4/N");
+
+	assertEqual(a - 1, 0xdeadbeae, "int64_t sub support 1/N");
+	assertEqual(b - 1, 0xffffffff, "int64_t sub support 2/N");
+	assertEqual(b - b, 0x00000000, "int64_t sub support 3/N");
+	assertEqual(c - b, 0xffffffffffffffff, "int64_t sub support 4/N");
+	assertEqual(d - d, 0x00000000, "int64_t sub support 5/N");
+}
+
+// TODO Add C file to test "if (int64_t)"
+
+static void testUnaryOps() {
+	long long a = 0x0;
+	long long b = 0x1;
+	long long c = 0x0000000100000000;
+	long long d = 0xffffffffffffffff;
+
+	assertEqual(-a, 0, "int64_t neg support 1/N");
+	assertEqual(-b, 0xffffffffffffffff, "int64_t neg support 2/N");
+	assertEqual(-c, 0xffffffff00000000, "int64_t neg support 3/N");
+	assertEqual(-d, 0x00000001, "int64_t neg support 4/N");
+
+	assertEqual(!a, true, "int64_t lnot support 1/N");
+	assertEqual(!b, false, "int64_t lnot support 2/N");
+	assertEqual(!c, false, "int64_t lnot support 3/N");
+	assertEqual(!d, false, "int64_t lnot support 4/N");
 }
 
 static void testDump() {
@@ -55,15 +95,10 @@ static void testDump() {
 	dump(t);
 }
 
-// Test 64-bit integers arithmetics
-
-void webMain()
-{
+void webMain() {
 	testShiftOps();
 	testBitwiseOps();
+	testAddSubOps();
+	testUnaryOps();
 	testDump();
-	//long long e = a + d;
-	//assertEqual(e, 0x2deadbeaf, "64-bit integer support 4/N");
-	//long long f = e - 1;
-	//assertEqual(f, 0x2deadbeae, "64-bit integer support 5/N");
 }
