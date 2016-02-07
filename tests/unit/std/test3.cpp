384397250446842d27c84c95e17e05c97019496a
Alessandro Pignotti <a.pignotti@sssup.it>
2016-02-07 13:31:27 +0100

    Add test for std::string::insert

--- tests/unit/std/test3.cpp
+++ tests/unit/std/test3.cpp
@@ -13,4 +13,8 @@ void webMain()
 	std::string str2("test");
 	assertEqual(str2.size(), 4u, "String construction 1/2");
 	assertEqual(str2.capacity(), 9u, "String construction 2/2");
+	str2.insert(0, "pre", 3);
+	assertEqual(str2, std::string("pretest"), "String insertion 1/2");
+	str2.insert(0, str2.data()+3, 4);
+	assertEqual(str2, std::string("testpretest"), "String insertion 2/2");
 }
