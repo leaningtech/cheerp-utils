7bc247f50b8a6f8c15862a9a9eb1e025a103ecf2
Alessandro Pignotti <a.pignotti@sssup.it>
2016-02-09 18:19:52 +0100

    Add test for special chars in literal strings

---
+++ tests/unit/codegen/escapes.cpp
@@ -0,0 +1,11 @@
+//===---------------------------------------------------------------------===//
+//	Copyright 2016 Leaning Technlogies
+//===----------------------------------------------------------------------===//
+
+#include <tests.h>
+
+void webMain()
+{
+	client::console.log("V\bery evil\t string\n\xff");
+	// No tests, but it should run
+}
