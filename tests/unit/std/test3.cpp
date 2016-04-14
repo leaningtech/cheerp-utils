e62b861541a58c3134b5c5d74d69e0875b8653d4
Alessandro Pignotti <a.pignotti@sssup.it>
2016-04-14 17:58:29 +0200

    Add another test for std::string

--- tests/unit/std/test3.cpp
+++ tests/unit/std/test3.cpp
@@ -1,5 +1,5 @@
 //===---------------------------------------------------------------------===//
-//	Copyright 2013 Leaning Technlogies
+//	Copyright 2013,2016 Leaning Technlogies
 //===----------------------------------------------------------------------===//
 
 #include <tests.h>
@@ -12,9 +12,12 @@ void webMain()
 	assertEqual(str.capacity(), 0u, "String empty construction 2/2");
 	std::string str2("test");
 	assertEqual(str2.size(), 4u, "String construction 1/2");
-	assertEqual(str2.capacity(), 9u, "String construction 2/2");
+	assertEqual(str2.capacity(), 15u, "String construction 2/2");
 	str2.insert(0, "pre", 3);
 	assertEqual(str2, std::string("pretest"), "String insertion 1/2");
 	str2.insert(0, str2.data()+3, 4);
 	assertEqual(str2, std::string("testpretest"), "String insertion 2/2");
+	std::string a("BBBBB-CCC-");
+	a.append("AAAAAA",6);
+	assertEqual(a, std::string("BBBBB-CCC-AAAAAA"), "String concatenation");
 }
