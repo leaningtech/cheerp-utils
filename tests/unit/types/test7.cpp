cb38314d2e600ffcc2c6aa48dabe4bb1292e2181
Alessandro Pignotti <a.pignotti@sssup.it>
2016-06-02 12:52:11 +0200

    Add test for defaulted constructors

--- tests/unit/types/test7.cpp
+++ tests/unit/types/test7.cpp
@@ -5,6 +5,7 @@
 #include <tests.h>
 
 bool testSuccessful = false;
+bool testSuccessful2 = false;
 
 class [[cheerp::jsexport]] JsStruct
 {
@@ -17,8 +18,26 @@ public:
 	}
 	void test()
 	{
-		if(b  == 42)
+		if(b == 42)
 			testSuccessful = true;
+	}
+};
+
+class [[cheerp::jsexport]] JsStruct2
+{
+private:
+	float a;
+	int b;
+public:
+	JsStruct2() = default;
+	void set_b(int i)
+	{
+		b = i;
+	}
+	void test()
+	{
+		if(b == 43)
+			testSuccessful2 = true;
 	}
 };
 
@@ -26,5 +45,7 @@ void webMain()
 {
 	//Test JS-layout struct
 	__asm__("var a=new JsStruct(3.0,42); a.test()");
-	assertEqual(testSuccessful, true, "JS interoperability using [[cheerp::jsexport]]/__asm__");
+	assertEqual(testSuccessful, true, "JS interoperability using [[cheerp::jsexport]]/__asm__ 1/2");
+	__asm__("var a=new JsStruct2(); a.set_b(43); a.test()");
+	assertEqual(testSuccessful2, true, "JS interoperability using [[cheerp::jsexport]]/__asm__ 2/2");
 }
