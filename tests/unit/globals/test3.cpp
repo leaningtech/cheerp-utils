5e2352618248c4911ab50f37ff8d5c9f1eeee6b5
Alessandro Pignotti <a.pignotti@sssup.it>
2016-09-06 16:38:50 +0200

    Fix a test case that was broken since the beginning

--- tests/unit/globals/test3.cpp
+++ tests/unit/globals/test3.cpp
@@ -4,12 +4,12 @@
 
 #include <tests.h>
 
-static char *var1[] = { 0 };
+static char *var1[] = { "\0" };
 
 char **var2 = &var1[0];
 
 void webMain()
 {
 	char* a=*var2;
-	assertEqual<char>(*a, 0, "Access to pointers of global arrays");
+	assertEqual<char>(*a, '\0', "Access to pointers of global arrays");
 }
