8369a91af1ba7d1e537919d427b5bba8b21f84f9
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-01-11 10:28:13 +0100

    add Object.set_() method that accepts basic types

--- include/client/jsobject.h
+++ include/client/jsobject.h
@@ -21,6 +21,8 @@
 #ifndef _CHEERP_JSOBJECT_H_65a8f9e1
 #define _CHEERP_JSOBJECT_H_65a8f9e1
 
+#include <type_traits>
+
 namespace [[cheerp::genericjs]] client
 {
 
@@ -45,6 +47,8 @@ public:
 	// operator[] for arbitrary assignment can't be expressed as we can't have a pointer to an arbitrary member of an object
 	// We provide the following function instead
 	void set_(const client::String& name, Object* v);
+	template<typename T, typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
+	void set_(const client::String& name, T v);
 	bool hasOwnProperty(const client::String& name);
 	static Array* keys(Object*) [[cheerp::static]];
 	static Array* values(Object*) [[cheerp::static]];
