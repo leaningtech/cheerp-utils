87e3ba24a28732bd49b9f91808431d3fab71b20f
Alessandro Pignotti <a.pignotti@sssup.it>
2018-03-13 14:41:27 +0100

    Remove client/server support

--- tests/server/server1.cpp
+++
@@ -1,9 +0,0 @@
-#include <cheerp/server.h>
-#include <iostream>
-
-std::string serverTest(int i, float f) [[cheerp::server]]
-{
-	std::cout << "Expected -42: " << i << "\n";
-	std::cout << "Expected 0.0021: " << f << "\n";
-	return "ServerTest";
-}
