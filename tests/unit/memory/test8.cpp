971241ecda02fa2335d197b06bd620e4c51b70ec
Alessandro Pignotti <a.pignotti@sssup.it>
2018-03-22 11:02:08 +0100

    Expand the [[cheerp::noinit]] test

--- tests/unit/memory/test8.cpp
+++ tests/unit/memory/test8.cpp
@@ -20,7 +20,10 @@ void webMain()
 	A* a=new A[2] [[cheerp::noinit]];
 	a[1].a = 42;
 	assertEqual(a[1].a, 42, "New array with [[cheerp::noinit]]");
-	/*A(*a2)[3]=new A[2][3] [[cheerp::noinit]];
+	A(*a2)[3]=new A[2][3] [[cheerp::noinit]];
 	a2[1][1].a = 43;
-	assertEqual(a2[1][1].a, 43, "New array with [[cheerp::noinit]] 1/N");*/
+	assertEqual(a2[1][1].a, 43, "New multi array with [[cheerp::noinit]]");
+	A* a3=new A [[cheerp::noinit]];
+	a3->a = 44;
+	assertEqual(a3->a, 44, "New with [[cheerp::noinit]]");
 }
