48827d972b5f6ebcea0ea7ddccfe631e50e2b8c5
Yuri Iozzelli <y.iozzelli@gmail.com>
2018-05-16 12:15:10 +0200

    Register number in asm statements now counts the output registers too

--- tests/unit/codegen/advancedasm.cpp
+++ tests/unit/codegen/advancedasm.cpp
@@ -9,13 +9,13 @@ void webMain()
 {
 	int a;
 	client::String* s = new client::String("testString");
-	__asm__("1+%0.length" : "=r"(a) : "r"(s));
+	__asm__("1+%1.length" : "=r"(a) : "r"(s));
 	assertEqual(a, 11, "Advanced inline asm 1/3");
 	double b;
-	__asm__("0.5+%0.length" : "=r"(b) : "r"(s));
+	__asm__("0.5+%1.length" : "=r"(b) : "r"(s));
 	assertEqual(b, 10.5, "Advanced inline asm 2/3");
 	client::String* s2;
-	__asm__("%0" : "=r"(s2) : "r"(s));
+	__asm__("%1" : "=r"(s2) : "r"(s));
 	assertEqual(s2, s, "Advanced inline asm 3/3");
 	// Test clobbering
 	__asm__("var f=%0" : : "r"(3) : "f","g");
