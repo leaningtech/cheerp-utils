e251355aa8925dc0477f925679ee3a1fc047801b
Alessandro Pignotti <a.pignotti@sssup.it>
2019-08-27 09:14:01 +0200

    Fix tests now that 64-bit ints are properly little endian

--- tests/unit/types/union64.cpp
+++ tests/unit/types/union64.cpp
@@ -16,8 +16,8 @@ void webMain()
 	//Test union with uint64_t
 	U u;
 	u.d = M_PI;
-	assertEqual<uint64_t>(u.u64, 6072027779779076603, "Access to union with unt64_t member 1/2");
+	assertEqual<uint64_t>(u.u64, 4614256656552045848, "Access to union with unt64_t member 1/2");
 	u.d = 0.0;
-	u.u64 = 6072027779779076603;
+	u.u64 = 4614256656552045848;
 	assertEqual<double>(u.d, M_PI, "Access to union with unt64_t member 2/2");
 }
