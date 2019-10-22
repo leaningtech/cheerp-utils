e53f043969247ed4eb34e1aad13957124b96343a
Carlo Piovesan <carlo.piovesan@gmail.com>
2019-10-22 17:26:38 +0200

    Add empty test

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -97,7 +97,7 @@ pre_executer_tests = ['unit/downcast/test1.cpp',
 	]
 common_tests = pre_executer_tests + [
 		'unit/std/gettimeofday.cpp','unit/std/chrono.cpp', 'unit/ffi/test3.cpp',
-		'unit/closures/test4.cpp',
+		'unit/closures/test4.cpp','unit/codegen/empty.cpp'
 		]
 genericjs_tests = common_tests + [
 		'unit/dom/test1.cpp','unit/dom/test2.cpp','unit/dom/test3.cpp','unit/dom/test4.cpp',
