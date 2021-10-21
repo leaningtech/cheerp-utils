a6abb197c04879744b69efe52f0d83d25487e64b
Carlo Piovesan <piovesan.carlo@gmail.com>
2021-10-21 09:55:17 +0200

    Fix abs test: add checks also on i8.abs

--- tests/unit/codegen/abs.cpp
+++ tests/unit/codegen/abs.cpp
@@ -12,7 +12,7 @@ int computeFunc(int A)
 	B *= A;
 	B *= A;
 	B = std::abs(B);
-	return B % 123123;
+	return B % T(123123);
 }
 
 int main()
@@ -20,9 +20,15 @@ int main()
 	int X = 0;
 	for (int i=-100; i<100; i++)
 		X += computeFunc<long long>(i) + computeFunc<long long>(i << 5) + computeFunc<long long>(i * 1e5 + 12);
-	assertEqual(X, 32038773, "Absolute value summation 1/2");
+	assertEqual(X, 32038773, "Absolute value summation 1/3");
+
 	int Y = 0;
 	for (int i=-100; i<100; i++)
 		Y += computeFunc<int>(i) + computeFunc<int>(i << 5) + computeFunc<int>(i * 1e5 + 12);
-	assertEqual(Y, 33131675, "Absolute value summation 2/2");
+	assertEqual(Y, 33131675, "Absolute value summation 2/3");
+
+	int Z = 0;
+	for (int i=-100; i<100; i++)
+		Z += computeFunc<char>(i) + computeFunc<char>(i << 5) + computeFunc<char>(i * 1e5 + 12);
+	assertEqual(Z, 3542, "Absolute value summation 3/3");
 }
