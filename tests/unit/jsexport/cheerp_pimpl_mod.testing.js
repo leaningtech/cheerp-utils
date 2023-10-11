1ee1c045579d201974fa838c737033eccb1aede5
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2023-10-11 13:32:55 +0200

    fix heap use after free

--- tests/unit/jsexport/cheerp_pimpl_mod.testing.js
+++ tests/unit/jsexport/cheerp_pimpl_mod.testing.js
@@ -3,8 +3,8 @@ function onInstantiation(_)
 	var instance = new _.JSExportedWrapperWithDeleter(21);
 	instance.insert(45);
 	instance.insert(2134);
-	instance.deleter();
 	var X = instance.totalInserted();
+	instance.deleter();
 	if (_.CounterAlive.numberAlive() == 0)
 	       console.log("JSExport deleter check 1/1", "SUCCESS");
 	else
