58f94294a65e0e44314f0bb954e366d6d816dff2
Chen Steenvoorden <dutchen18@gmail.com>
2024-03-04 11:58:03 +0100

    Add option to use old clientlib by defining USE_OLD_CLIENTLIB

--- include/client/jsexception.h
+++ include/client/jsexception.h
@@ -1,39 +1,5 @@
-//===-- jsexception.h - C++ Wrapper for JS exceptions --------------===//
-//
-//                     Cheerp: The C++ compiler for the Web
-//
-// This file is distributed under the Apache License v2.0 with LLVM Exceptions.
-// See LICENSE.TXT for details.
-//
-// Copyright 2011-2023 Leaning Technologies
-//
-//===----------------------------------------------------------------------===//
-
-#ifndef _CHEERP_JSEXCEPTION
-#define _CHEERP_JSEXCEPTION
-
-#include "cheerp/jsobject.h"
-
-#ifdef __cplusplus
-
-namespace [[cheerp::genericjs]] cheerp
-{
-
-class JSException {
-	client::Object* inner;
-	public:
-		JSException(client::Object* e): inner(e)
-		{
-		}
-		client::Object* get()
-		{
-			return inner;
-		}
-};
-
-
-} //End of namespace cheerp
-
-#endif
-
+#ifdef USE_OLD_CLIENTLIB
+#include "old/jsexception.h"
+#else
+#include "new/jsexception.h"
 #endif
